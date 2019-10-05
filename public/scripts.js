c26b2e02f5c31f39625b94e1fd2e07fe80684881
Majidsp <majid_sp2007@yahoo.com>
2019-10-05 13:18:25 +0200

    Completed client side Javascript for Canvas.

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,40 @@
 (function(){
+
     const context = document.getElementById('canv').getContext('2d');
-    context.strokeStyle = '#900';
-    context.beginPath();
-    context.moveTo(100, 0);
-    context.lineTo(0, 200);
-    context.moveTo(100, 0);
-    context.lineTo(200, 200);
-    context.lineTo(0, 200);
-    context.stroke();
+    const drawBox = document.getElementById('canv');
+    let lastX;
+    let lastY;
+    let mousePressed = false;
 
+    //Events
+    drawBox.addEventListener('mousedown', e => {
+        mousePressed = true;
+        draw(e.pageX - drawBox.offsetLeft, e.pageY - drawBox.offsetTop, false);
+    });
+    drawBox.addEventListener('mousemove', e => {
+        if(mousePressed) {
+            draw(e.pageX - drawBox.offsetLeft, e.pageY - drawBox.offsetTop, true);
+        }
+    });
+    drawBox.addEventListener('mouseup', () => {
+        mousePressed = false;
+    });
+    drawBox.addEventListener('mouseleave', () => {
+        mousePressed = false;
+    });
 
+    const draw = (x, y, mouseDown) => {
+        if(mouseDown) {
+            context.strokeStyle = 'white';
+            context.lineWidth = 1;
+            context.lineJoin = 'round';
+            context.beginPath();
+            context.moveTo(lastX, lastY);
+            context.lineTo(x, y);
+            context.stroke();
+        }
+        lastX = x;
+        lastY = y;
+    };
 
 })();
