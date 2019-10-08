c459ddc65abcce3c7f1854f617dc28330e04e5b3
Majidsp <majid_sp2007@yahoo.com>
2019-10-08 16:38:30 +0200

    tested encode module succesfully.

--- queries.sql
+++ queries.sql
@@ -15,15 +15,28 @@
 -- ALTER TABLE petition ADD COLUMN firstname varchar(255) NOT NULL;
 -- ALTER TABLE petition ADD COLUMN lastname varchar(255) NOT NULL;
 -- ALTER TABLE petition ADD COLUMN signature varchar NOT NULL;
--- ALTER TABLE petition ADD COLUMN timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+-- ALTER TABLE users ADD COLUMN timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
 
 
 -- SELECT * FROM petition;
+SELECT * FROM users;
+
 -- SELECT COUNT(*) AS NUMBEROFSIGNERS FROM petition;
-SELECT FIRSTNAME, LASTNAME FROM petition;
+-- SELECT FIRSTNAME, LASTNAME, TIMESTAMP FROM petition;
 
 -- DELETE FROM petition;
 
 
 -- SELECT COUNT(*) FROM petition;
 -- SELECT SIGNATURE FROM petition WHERE id = 2;
+
+-- 
+-- CREATE TABLE USERS (
+--    id SERIAL primary key,
+--    firstname VARCHAR(255) NOT NULL,
+--    lastname VARCHAR(255) NOT NULL,
+--    email VARCHAR(255) NOT NULL UNIQUE,
+--    password VARCHAR(255) NOT NULL
+-- );
+
+-- DROP TABLE users;
